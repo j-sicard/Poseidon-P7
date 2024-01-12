6a451155e583b0f3d8327db6fab810b7b3c7649a
J-sicard <ptiptilu@hotmail.fr>
2024-01-12 16:07:37 +0100

    first commit

---
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -0,0 +1,17 @@
+package com.nnk.springboot.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.beans.factory.annotation.Required;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "bidlist")
+public class BidList {
+    // TODO: Map columns in data table BIDLIST with corresponding java fields
+}
